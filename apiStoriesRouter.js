189b54e247737db2e2f8985bd10c02b3517fa6e5
ericpcho <ericpcho@gmail.com>
2017-09-15 12:57:25 -0700

    added routing and fixed our get function

---
+++ apiStoriesRouter.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+
+const bodyParser = require('body-parser');
+const jsonParser = bodyParser.json();
+
+const { DATABASE } = require('./config');
+const knex = require('knex')(DATABASE);
+
+// ADD YOUR ENDPOINTS HERE
+
+router.post('/', (req, res) => {
+  let newId;
+  console.log('running post');
+  knex
+    .insert({
+      title: req.body.title,
+      url: req.body.url,
+      //votes: 0
+    })
+    .into('news')
+    .returning('id')
+    .then(([id]) => {
+      newId = id;
+      let promises = [];
+  
+      req.body.tags.forEach(tag => {
+        const promise = knex('news_tags')
+          .insert({
+            news_id: newId,
+            tags_id: tag
+          })
+          promises.push(promise);
+      });
+      return Promise.all(promises);
+    })
+    .then(() => {
+      res.location(`/api/stories/${newId}`).sendStatus(201);
+    })
+    
+});
+
+// router.get('/', (req, res) => {
+//   console.log('running get');
+//   knex.select()
+//     .from('news')
+//     .orderBy('title')
+//     .then(results => res.json(results));
+// });
+
+router.get('/', (req, res) => {
+  console.log('running get');
+  knex.select('news.id', 'title', 'url', 'votes', 'tags.id', 'tag', 'tags_id as tagId', 'news_id')
+    .from('news')
+    .join('news_tags', 'news.id', 'news_tags.news_id')
+    .join('tags', 'news_tags.tags_id', 'tags.id')
+    .orderBy('news_id')
+    .then(results => {
+      const hydRes = [], lookup = {};
+      for (let story of results) {
+        if (!lookup[story.news_id]) {
+          lookup[story.news_id] = {
+            id: story.news_id,
+            title: story.title,
+            url: story.url,
+            votes: story.votes,
+            tags: []
+          };
+          hydRes.push(lookup[story.news_id]);
+        }
+        lookup[story.news_id].tags.push({
+          id: story.tagId,
+          tag: story.tag
+        });
+      };
+      res.json(hydRes);
+    });
+});
+
+router.put('/:id', (req, res) => {
+  console.log('running put', req.params.id);
+  knex('news')
+    .where('id', '=', req.params.id)
+    .increment('votes', 1)
+    .then(res.sendStatus(204));
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,97 +4,13 @@ require('dotenv').config()
 
 const express = require('express');
 const morgan = require('morgan');
-const bodyParser = require('body-parser');
-
-const { DATABASE, PORT } = require('./config');
-const knex = require('knex')(DATABASE);
-
+const { PORT } = require('./config');
 const app = express();
+const apiStoriesRouter = require('./apiStoriesRouter')
 
 app.use(morgan(':method :url :res[location] :status'));
+app.use('/api/stories', apiStoriesRouter);
 
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
-
-// ADD YOUR ENDPOINTS HERE
-let newId;
-app.post('/api/stories', (req, res) => {
-  console.log('running post');
-  knex
-    .insert({
-      title: req.body.title,
-      url: req.body.url,
-      //votes: 0
-    })
-    .into('news')
-    .returning('id')
-    .then(([id]) => {
-      newId = id;
-      let promises = [];
-  
-      req.body.tags.forEach(tag => {
-        const promise = knex('news_tags')
-          .insert({
-            news_id: newId,
-            tags_id: tag
-          })
-          promises.push(promise);
-      });
-      return Promise.all(promises);
-    })
-    .then(() => {
-      res.location(`/api/stories/${newId}`).sendStatus(201);
-    })
-    
-});
-
-// app.get('/api/stories', (req, res) => {
-//   console.log('running get');
-//   knex.select()
-//     .from('news')
-//     .orderBy('title')
-//     .then(results => res.json(results));
-// });
-
-app.get('/api/stories', (req, res) => {
-  console.log('running get');
-  knex.select('news.id', 'title', 'url', 'votes', 'tags.id', 'tag', 'tags_id', 'news_id')
-    .from('news')
-    .join('news_tags', 'news.id', 'news_tags.news_id')
-    .join('tags', 'news_tags.tags_id', 'tags.id')
-    .orderBy('news_id')
-    .then(results => {
-      const hydRes = [], lookup = {};
-      for (let story of results) {
-        if (!lookup[story.news_id]) {
-          lookup[story.news_id] = {
-            news_id: story.news_id,
-            title: story.title,
-            url: story.url,
-            votes: story.votes,
-            tags: []
-          };
-          hydRes.push(lookup[story.id]);
-        }
-        lookup[story.news_id].tags.push({
-          id: story.tags_id,
-          tag: story.tag
-        });
-      };
-      res.json(hydRes);
-    });
-});
-
-app.put('/api/stories/:id', (req, res) => {
-  console.log('running put', req.params.id);
-  knex('news')
-    .where('id', '=', req.params.id)
-    .increment('votes', 1)
-    .then(res.sendStatus(204));
-})
 
 let server;
 function runServer(){
